5eccc6154c7b61d6a2cf8dc656071201903866cb
Lewis <lewis.njuma@student.moringaschool.com>
2025-04-22 12:25:31 +0300

    added an image in photocard

--- src/components/PhotoCard.jsx
+++ src/components/PhotoCard.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const PhotoCard = ({ photo }) => {
   return (
     <div className="photo-card">
+      <img src={photo.urls.small} alt={photo.alt_description} />
      
     </div>
   );
