352cecbdb19bd75bee64ed818bac374f3bcb3552
Lewis <lewis.njuma@student.moringaschool.com>
2025-04-22 12:29:50 +0300

    Your message

--- src/components/PhotoCard.jsx
+++ src/components/PhotoCard.jsx
@@ -4,7 +4,9 @@ const PhotoCard = ({ photo }) => {
   return (
     <div className="photo-card">
       <img src={photo.urls.small} alt={photo.alt_description} />
-     
+      <p>
+        <a href={photo.user.links.html} target="_blank" rel="noopener noreferrer">{photo.user.name}</a>
+      </p>
     </div>
   );
 };
