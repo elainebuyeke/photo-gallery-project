8a70b3f9af53fcdab844753cad2b9ebf7dbbf6b6
Stacey jepchumba <satceyjepchumbarr@gamil.com>
2025-04-22 12:24:33 +0300

    photo grid

---
+++ src/components/PhotoGrid.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import PhotoCard from './PhotoCard';
+
+const PhotoGrid = ({ photos }) => {
+  return (
+    <div className="photo-grid">
+      
+    </div>
+    
+  );
+};
+
+export default PhotoGrid;
