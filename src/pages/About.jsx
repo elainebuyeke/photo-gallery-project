350c7198e17bc1341f3190672315a521594cb28c
Elaine <omwengaelaine@gmail.com>
2025-04-22 14:15:45 +0300

    create about section

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="about">
+      <h2>About This App</h2>
+      <p>This is a photo gallery app. You can view random photos or search by keyword.</p>
+    </div>
+  );
+};
+
+export default About;
